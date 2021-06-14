09ee22600eef362c89ef088feb30da0aebb60101
Ramii Ahmed <ramy@ahmedramy.com>
2021-06-14 06:59:13 +0000

    lmi: Adapt to Fluid

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,13 +1,13 @@
 #
-# Copyright (C) 2021 The LineageOS Project
+# Copyright (C) 2021 The fluidOS Project
 #
 # SPDX-License-Identifier: Apache-2.0
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_lmi.mk
+    $(LOCAL_DIR)/fluid_lmi.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_lmi-user \
-    lineage_lmi-userdebug \
-    lineage_lmi-eng
+    fluid_lmi-user \
+    fluid_lmi-userdebug \
+    fluid_lmi-eng
